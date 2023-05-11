97887c8befe9345b4d1443a8e76092c4510fd129
MATEO COTTET <47168146@ort>
2023-05-11 15:54:32 -0300

    se empezo el tp

---
+++ Models/equipos.cs
@@ -0,0 +1,15 @@
+static public class Equipos
+{
+public static List<string> ListaEquipos {get; private set;}
+public static List<string> ListaMedias {get; private set;}
+public static List<string> ListaPantalones {get; private set;}
+public static List <string> ListaRemeras {get; private set;}
+public static Dictionary <string, Indumentaria> EquiposIndumentaria {get; private set;}
+
+public static bool IngresarIndumentaria(string EquipoSeleccionado, Indumentaria item) {
+
+bool existe = false;
+    return existe; 
+}
+
+}
---
+++ Models/indumentaria.cs
@@ -0,0 +1,6 @@
+public class Indumentaria
+{
+public string Medias {get; private set;}
+public string Pantalon {get; private set;}
+public string Camiseta {get; private set;}
+}
